03b48e352e6619304f55d5301b4266ae3934461c
Nikita <ed.kahr.19063@gmail.com>
2024-11-06 22:32:15 +0300

    update url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-	baseURL: 'http://localhost:5000/api', // Укажите правильный базовый URL вашего API
+	// baseURL: 'http://localhost:5000/api',
+	baseURL: 'https://tomorrowme-ed751e363316.herokuapp.com/api',
 })
